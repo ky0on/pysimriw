7b0eec07642538a75cb8dd47dd68b22633d9391f
kyon <kyon.0308@gmail.com>
2017-10-24 15:31:04 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='pysimriw',
+    packages=find_packages(),
+    version='v0.1',
+    description="SIMRIW with Python",
+    author='kyon',
+    url='https://bitbucket.org/ky0on/pysimriw',
+    classifiers=[],
+)
