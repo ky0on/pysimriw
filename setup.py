fe5b0999d3fd16228119e90c9ab1376c17a65c3c
kyon <kyon.0308@gmail.com>
2017-11-09 23:27:38 +0900

    add test

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='pysimriw',
     packages=find_packages(),
-    version='v0.1',
+    version='v0.2',
     description="SIMRIW with Python",
     author='kyon',
     url='https://bitbucket.org/ky0on/pysimriw',
